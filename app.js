b26b9df41f766727486c3e39302862f185d81c19
Vinicius Silva <vinicius.nascimento.silva@usp.br>
2014-12-01 14:23:44 -0200

    pagina web

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var rpgApp = angular.module('rpgApp', [ 'ngRoute', 'restangular',
+	'ui.bootstrap', 'infinite-scroll', ]);
+
+rpgApp.config([ '$routeProvider', function($routeProvider) {
+    $routeProvider.when('/index', {
+	templateUrl : 'perfil/index.html',
+	controller : 'perfilIndexCtrl'
+    }).otherwise({
+	redirectTo : '/index'
+    });
+} ]);
+
+/*rpgApp.config([ 'RestangularProvider', function(RestangularProvider) {
+    RestangularProvider.setBaseUrl("http://localhost/rpg/");
+    RestangularProvider.setRestangularFields({
+	selfLink : "_links.self.href"
+    });
+    RestangularProvider.setDefaultHeaders({
+	'Content-Type' : 'application/json'
+    });
+} ]);*/
