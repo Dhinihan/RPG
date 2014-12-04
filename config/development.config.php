a7f8c9f395f6beb65a3950c460dd820fc16393e6
vinicius <vinicius.nascimento.silva@usp.br>
2014-12-04 19:05:40 -0300

    heroku

--- config/development.config.php
+++ config/development.config.php
@@ -6,8 +6,6 @@
 return array(
     // Development time modules
     'modules' => array(
-        'ZF\Apigility\Admin',
-        'ZF\Configuration',
     ),
     // development time configuration globbing
     'module_listener_options' => array(
